cf0ce811957e8643d919e146b183ba3f4563f14c
NVital14 <noemimonvital4567@gmail.com>
2024-09-30 12:09:59 +0100

    As primeiras coisas que fizemos do projeto

---
+++ ProjetoCD - Curriculo_Digital/src/projetocd/curriculo_digital/ProjetoCDCurriculo_Digital.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetocd.curriculo_digital;
+import javax.swing.*;
+/**
+ *
+ * @author noemi
+ */
+public class ProjetoCDCurriculo_Digital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Interface janela = new Interface();
+       janela.setVisible(true);
+    }
+    
+}
